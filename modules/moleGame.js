088030337fad521738808d81b3a2278ca73651e2
unknown <euniceancajima@gmail.com>
2022-03-13 03:33:57 -0500

    comparación de aciertos y eliminar eventos al reiniciar juego

--- modules/moleGame.js
+++ modules/moleGame.js
@@ -3,23 +3,39 @@ const scoreMole = document.querySelector('#score-mole');
 const timeMole = document.querySelector('#time-mole');
 export const square = document.querySelectorAll('.square');
 
-export const btnStart = document.querySelector('.btn-start-mole');
+export const btnStart = document.querySelector('.btn-start');
+const btnReset = document.querySelector('.btn-reset');
 let squeareRandom = null;
+let timeInterval = null;
+
+const removeRandomMole = (() => {
+    square.forEach(data => data.classList.remove('moleActive'));
+});
 
 const showRandomMole = (() => {
-    square.forEach(data => data.classList.remove('moleActive'));
+    removeRandomMole();
     squeareRandom = square[Math.floor(Math.random() * 9)];
     squeareRandom.classList.add('moleActive');
-})
-export const showMole = (() => {
-    let timeInterval = null;
+});
+/* Comprobamos si acerto en lo seleccionado, necesario también para eliminar eliminar eventos */
+const match = ((e) => {
+    if (e.target.id ===  squeareRandom.id) {
+        alert("Tengo tanto odio, que me carcome el alma");
+    }
+});
 
+export const showMole = (() => {
     square.forEach((elm) => {
-        elm.addEventListener('mousedown', ()=> {
-            if (elm.id ===  squeareRandom.id) {
-                alert("Tengo tanto odio, que me carcome el alma");
-            }
-        });
+        elm.addEventListener('mousedown', match);
     });
+    btnReset.addEventListener('click', () => {
+        square.forEach((elm) => {
+            elm.removeEventListener('mousedown', match);
+        });
+        removeRandomMole();
+        clearInterval(timeInterval);
+        btnStart.disabled = false;
+    });
+
     timeInterval = setInterval(showRandomMole, 800);
 });
