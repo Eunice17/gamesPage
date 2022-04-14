20a9e511280a2cdb6617cdbe505fd66637326f37
unknown <euniceancajima@gmail.com>
2022-04-14 11:13:45 -0500

    Agregando bloques y bloque jugador

--- modules/breakGame.js
+++ modules/breakGame.js
@@ -1,5 +1,60 @@
 const marco = document.querySelector('.grid-break');
-const block = document.createElement('div');
+const blockWidth = 100;
+const blockHeight = 6;
+const startGamer = [40, 1];
 
-block.classList.add('block');
-marco.appendChild(block);
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRigth = [xAxis + blockWidth ,yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRigth = [xAxis + blockWidth, yAxis + blockHeight];
+    }
+}
+
+const blocks = [
+    new Block(2, 90),
+    new Block(26, 90),
+    new Block(51, 90),
+    new Block(75, 90),
+    new Block(2, 80),
+    new Block(26, 80),
+    new Block(51, 80),
+    new Block(75, 80),
+    new Block(2, 70),
+    new Block(26, 70),
+    new Block(51, 70),
+    new Block(75, 70),
+    new Block(2, 60),
+    new Block(26, 60),
+    new Block(51, 60),
+    new Block(75, 60),
+    /* new Block(2, 91),
+    new Block(80, 91),
+    new Block(157, 91),
+    new Block(235, 91), */
+]
+
+console.log(blocks[0]);
+
+const createBlock = (() => {
+    for (let i = 0; i< blocks.length; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.backgroundColor = "var(--verde)";
+        block.style.left = blocks[i].bottomLeft[0]+'%';
+        block.style.bottom = blocks[i].bottomLeft[1]+'%';
+        marco.appendChild(block);
+    }
+});
+
+const createGamer = (() => {
+    const block = document.createElement('div');
+        block.classList.add('blockGamer');
+        block.style.backgroundColor = "var(--nav)";
+        block.style.left = startGamer[0]+'%';
+        block.style.bottom = startGamer[1]+'%';
+        marco.appendChild(block);
+});
+createGamer();
+createBlock();
